82e1fd4a379986a13d2eb77a4e00696df3c55f02
Morketh <admmorketh@gmail.com>
2014-03-15 23:02:46 -0500

    initial commit

---
+++ config/config-protected.php
@@ -0,0 +1,10 @@
+<?php
+$db = array(
+'db_host'         => 'localhost',
+'db_port'         => '3306',
+'db_username'     => 'admiral',
+'db_password'     => 'stu475r4.0.0.5',
+'db_name'         => 'realmd-account',
+'db_encoding'     => 'utf8',
+);
+?>
